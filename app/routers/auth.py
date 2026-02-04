9e507eada26e9e9d55a006fa08093178aad0f541
Atharv Kumar <172103387+zokomom@users.noreply.github.com>
2026-02-05 00:06:25 +0530

    Added jwt token authentication with secret

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,5 @@
 from ..database import get_db
-from .. import schemas,models,utils
+from .. import schemas,models,utils,oauth2 
 from fastapi import APIRouter,Depends,Response,status,HTTPException
 from sqlalchemy.orm import Session
 
@@ -19,4 +19,5 @@ def login(user_credentials:schemas.UserLogin,db:Session=Depends(get_db)):
     if not utils.verify_password(user_credentials.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
 
-    return {"status" : "logged in"}
+    access_token=oauth2.create_access_token(data={"user_id":user.user_id})
+    return {"access_token" : access_token,"token_type": "bearer"}
