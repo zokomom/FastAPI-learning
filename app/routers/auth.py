c6afc1f278aa3a239b06d80ff9a28aff6f145968
Atharv Kumar <172103387+zokomom@users.noreply.github.com>
2026-02-05 17:44:32 +0530

    refactor: accept login credentials via OAuth2PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,16 +2,16 @@ from ..database import get_db
 from .. import schemas,models,utils,oauth2 
 from fastapi import APIRouter,Depends,Response,status,HTTPException
 from sqlalchemy.orm import Session
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router=APIRouter(
     tags=['authentication']
 )
 
 @router.post("/login")
-def login(user_credentials:schemas.UserLogin,db:Session=Depends(get_db)):
+def login(user_credentials:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(get_db)):
 
-    user = db.query(models.Users).filter(models.Users.email==user_credentials.email).first()
+    user = db.query(models.Users).filter(models.Users.email==user_credentials.username).first()
 
     if not user :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
@@ -20,4 +20,5 @@ def login(user_credentials:schemas.UserLogin,db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
 
     access_token=oauth2.create_access_token(data={"user_id":user.user_id})
+
     return {"access_token" : access_token,"token_type": "bearer"}
