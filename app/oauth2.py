262dec0cd1e798c985860bf657fd6bca34cbf8a5
Atharv Kumar <172103387+zokomom@users.noreply.github.com>
2026-02-06 23:35:13 +0530

    refactor: enhance user retrieval in get_current_user by adding database dependency

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,7 +2,9 @@ from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer
-from . import schemas
+from . import schemas,models
+from sqlalchemy.orm import Session
+from .database import get_db
 
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl='login')
 
@@ -20,17 +22,22 @@ def create_access_token(data:dict):
 def verify_access_token(token:str,credentials_exception):
     try:
         payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+
         id : int = payload.get("user_id")
 
         if id is None:
             raise credentials_exception
         token_data=schemas.TokenData(id=id)
+
     except JWTError:
         raise credentials_exception
 
     return token_data
 
-def get_current_user(token:str=Depends(oauth2_scheme)):
+def get_current_user(token:str=Depends(oauth2_scheme),db:Session=Depends(get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Could not validate credentials", headers= {"WWW-Authenticate": "Bearer"})
-
-    return verify_access_token(token,credentials_exception)
+    token=verify_access_token(token,credentials_exception)
+    user=db.query(models.Users).filter(models.Users.user_id==token.id).first()
+    if not user :
+        raise credentials_exception
+    return user
