fe731aee1ae135fc8b04d6b79fbf2916cab89772
Atharv Kumar <172103387+zokomom@users.noreply.github.com>
2026-02-16 15:59:29 +0530

    feat: configure settings using environment variables for database and JWT

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL='postgresql+psycopg://postgres:root@localhost/social_media_database'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
