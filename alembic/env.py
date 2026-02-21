6818a29d8ff8f930e10744ce2d9b84682712edbf
Atharv Kumar <172103387+zokomom@users.noreply.github.com>
2026-02-21 18:22:24 +0530

    fix: correct sqlalchemy URL configuration in Alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from app.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqalchemy.url",f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}")
+config.set_main_option("sqlalchemy.url",f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
